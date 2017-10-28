937d668e2334096a24cf9174216c6c23aa568696
Turgon <hys.0515@163.com>
2017-10-28 14:54:28 +0800

    fix mQ
    use m1 instead

--- OneFlavour.m
+++ OneFlavour.m
@@ -156,10 +156,10 @@ Mseq=Sequence[M1,M2,M3,M4];Si=If[n1+n2>=n3+n4,M1+M2,M3+M4];
 Print["M1=",M1,"  M2=",M2,"  M3=",M3,"  M4=",M4];
 (*Print[$Context];*)
 (*DistributeDefinitions[M1,M2,M3,M4,\[Phi]1,\[Phi]2,\[Phi]3,\[Phi]4,EXPR,\[ScriptCapitalM]0,\[ScriptCapitalI]1,\[ScriptCapitalI]2,\[ScriptCapitalI]3,\[Omega]1S,\[Omega]2S];*)
-Which[MatchQ[mQ,Global`u], mQ = 0.045,MatchQ[mQ,Global`c], mQ = 4.23,MatchQ[mQ,Global`s], mQ = 0.749];
-(*Print["mQ=",mQ];Print[FullForm[c]];*)
-{{{n1,n2,n3,n4},{M1,M2,M3,M4},{m1}},ParallelTable[Sen=Ssqur^2;\[Omega]1=\[Omega]1S[Sen][M1,M2,M3,M4];\[Omega]2=\[Omega]2S[Sen][M1,M2,M3,M4];
-{Ssqur,EXPR[\[Omega]1,\[Omega]2,I1Option->OptionValue[FilterRules[{opt},I1Option]],I2Option->OptionValue[FilterRules[{opt},I2Option]],I3Option->OptionValue[FilterRules[{opt},I3Option]](*,Evaluate@FilterRules[{opt},Options[NIntegrate]]*)][\[Phi]1,\[Phi]2,\[Phi]3,\[Phi]4][mQ,M1,M2,M3,M4]},{Ssqur,Si+OptionValue[SRange][[1]],Si+OptionValue[SRange][[2]],OptionValue[SRange][[3]]},DistributedContexts->{"OneFlavour`Private`"}]}
+Which[MatchQ[m1,Global`u], m1 =(*Global`u= *)0.045,MatchQ[m1,Global`c], m1=(*Global`c =*) 4.23,MatchQ[m1,Global`s], m1 =(*Global`s=*)0.749];
+Print["m1=",m1];
+{{{n1,n2,n3,n4},{M1,M2,M3,M4},{mQ}},ParallelTable[Sen=Ssqur^2;\[Omega]1=\[Omega]1S[Sen][M1,M2,M3,M4];\[Omega]2=\[Omega]2S[Sen][M1,M2,M3,M4];Print[\[ScriptCapitalI]3[\[Omega]1,\[Omega]2][\[Phi]1,\[Phi]2,\[Phi]3,\[Phi]4][m1,M1,M2,M3,M4]];
+{Ssqur,EXPR[\[Omega]1,\[Omega]2,I1Option->OptionValue[FilterRules[{opt},I1Option]],I2Option->OptionValue[FilterRules[{opt},I2Option]],I3Option->OptionValue[FilterRules[{opt},I3Option]](*,Evaluate@FilterRules[{opt},Options[NIntegrate]]*)][\[Phi]1,\[Phi]2,\[Phi]3,\[Phi]4][m1,M1,M2,M3,M4]},{Ssqur,Si+OptionValue[SRange][[1]],Si+OptionValue[SRange][[2]],OptionValue[SRange][[3]]},DistributedContexts->{"OneFlavour`Private`"}]}
 ];
 
 Msum2[{mQ_,mq_},{n1_?IntegerQ,n2_?IntegerQ,n3_?IntegerQ,n4_?IntegerQ},opt:OptionsPattern[{SRange->{10^-3,2,0.01},Lambda->10^-6,Method->"BSW",DataDir->dirglo,gvalue->gglo,ProcessType->({{a,b},{b,a}}->{{a,b},{b,a}}),AssignQuark->{a->m1,b->m2},I1Option->OptionsPattern[],I2Option->OptionsPattern[],I3Option->OptionsPattern[]}]]:=
