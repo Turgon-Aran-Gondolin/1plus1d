beb9e2849b42110d318aadc066d1b7dcd910b997
Turgon <hys.0515@163.com>
2019-04-20 22:13:41 +0800

    4F 0011

--- OneFlavour.m
+++ OneFlavour.m
@@ -192,9 +192,9 @@ NIPVInt[intg_,{var_,var2_},pole_,opt:OptionsPattern[{PVMethod->"Differential"}]]
 		OptionValue[PVMethod]=="Subtraction",Message[PVInt::sub];Abort[],
 		OptionValue[PVMethod]=="Differential",
 		UsrReap[prog[((intg-intgp)/(var-pole)^2 )
-		Boole[(*(0<=pole<\[Lambda]&&(0<=var<pole/2||3pole/2<var<=1))||*)
+		Boole[(0<=pole<\[Lambda]&&(0<=var<pole/2||3pole/2<var<=1))||
 		(\[Lambda]<pole<=1-\[Lambda]&&(0<=var<pole-\[Lambda]||pole+\[Lambda]<var<=1))
-		(*||(1-\[Lambda]<pole<=1&&(0<=var<(3pole-1)/2||(pole+1)/2<var<=1))*)]
+		||(1-\[Lambda]<pole<=1&&(0<=var<(3pole-1)/2||(pole+1)/2<var<=1))]
 		+(intgp/(pole-1)-intgp/pole)(*Boole[\[Lambda]<pole<1-\[Lambda]]*),{var2,0,1},{var,0,1}]]
 		(*NIntegrate[(intgp/(pole-1)-intgp/pole),{var2,0,1}]+*)
 		(*prog[ex,{var2,var}\[Element]ImplicitRegion[((*(0<=pole<\[Lambda]&&(0\[LessEqual]var<pole/2||3pole/2<var\[LessEqual]1))||*)(\[Lambda]<pole\[LessEqual]1-\[Lambda]&&(0\[LessEqual]var<pole-\[Lambda]||pole+\[Lambda]<var\[LessEqual]1))(*||(1-\[Lambda]<pole\[LessEqual]1&&(0\[LessEqual]var<(3pole-1)/2||(pole+1)/2<var\[LessEqual]1))*))&&0<var2<1&&0<var<1,{var,var2}]]*)
